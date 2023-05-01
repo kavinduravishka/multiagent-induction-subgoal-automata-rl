593f46c3934e7a474ca0cc63ba400aa81a518b0c
kavinduravishka <kavinduravishka98sl@gmail.com>
2023-05-02 00:20:19 +0530

    Add nex features into utils for check differences between two arrays

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import numpy as np
 import json
+from colorama import init, Fore, Back, Style
 
 
 def get_param(param_dict, param_name, default_value=None):
@@ -60,3 +61,71 @@ def write_json_obj(obj, filepath):
     with open(filepath, 'w') as f:
         json.dump(obj, f)
 
+def diff_arrays(arr1,arr2, explaination_obj = None, explain_func = None):
+    explnation = ""
+
+    assert len(arr1) == len(arr2)
+    width = 8
+
+    l = len(arr1)
+
+    for i in range(l):
+        if arr1[i] != arr2[i]:
+            print(Fore.RED + "{:>{}}".format(arr1[i], width) + Style.RESET_ALL, end = "")
+            if explain_func and explaination_obj:
+                explnation += str(explain_func(explaination_obj[i]))
+        else:
+            print("{:>{}}".format(arr1[i], width), end = "")
+    
+    print("")
+
+    for i in range(l):
+        if arr1[i] != arr2[i]:
+            print(Fore.RED + "{:>{}}".format(arr2[i], width) + Style.RESET_ALL, end = "")
+        else:
+            print("{:>{}}".format(arr1[i], width), end = "")
+
+    print("\n")
+    print(explnation)
+    print("\n==========")
+    print()
+
+def check_diff_arrays_crucial_at_least_one_element(arr1, arr2, *crucial_elements):
+    assert len(arr1) == len(arr2)
+
+    crucial_change = False
+
+    l = len(arr1)
+
+    for i in range(l):
+        if arr1[i] != arr2[i] and (arr1[i] in crucial_elements or arr2[i] in crucial_elements):
+            crucial_change = True
+
+    return crucial_change
+
+def check_diff_arrays_crucial_only_one_element_arr2(arr1, arr2, *crucial_elements):
+    assert len(arr1) == len(arr2)
+
+    crucial_change = False
+
+    l = len(arr1)
+
+    for i in range(l):
+        if arr1[i] != arr2[i] and (arr1[i] not in crucial_elements and arr2[i] in crucial_elements):
+            crucial_change = True
+
+    return crucial_change
+
+
+def check_diff_arrays_crucial_both_elements(arr1, arr2, *crucial_elements):
+    assert len(arr1) == len(arr2)
+
+    crucial_change = False
+
+    l = len(arr1)
+
+    for i in range(l):
+        if arr1[i] != arr2[i] and (arr1[i] in crucial_elements and arr2[i] in crucial_elements):
+            crucial_change = True
+
+    return crucial_change
